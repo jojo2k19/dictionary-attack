734d92a8705c576017c2843bd903aa3673501ec3
jojo2k19 <jojo2k19@gmail.com>
2017-08-04 11:07:38 -0700

    Update dictionary-attack.js

--- dictionary-attack.js
+++ dictionary-attack.js
@@ -21,10 +21,10 @@ function checkPassword() {
       document.getElementById("results").innerHTML = "This is a weak password! It's just a word from the dictionary!";
       break;
   }
-    else if (password == wordsList[x] + "2017"){
-      document.getElementById("results").innerHTML = "This is a weak password!";
-      break;
-  }
+//     else if (password == wordsList[x] + "2017"){
+//       document.getElementById("results").innerHTML = "This is a weak password!";
+//       break;
+//   }
     else {
       document.getElementById("results").innerHTML = "Great job! This is a strong password!";
       break;
